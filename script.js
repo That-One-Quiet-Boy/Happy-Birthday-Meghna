4fe21a8d8522209de8020bee55528a8373c7ba1f
Ahnaf Shariar <fjfgjhguwhbxgh@gmail.com>
2025-03-03 17:05:39 +0600

    script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ var w = c.width = window.innerWidth,
 		hh = h / 2,
 		
 		opts = {
-			strings: [ 'Happy Birthday...,', 'Afrina!'],
+			strings: [ 'Happy Birthday...,', 'Meghna!'],
 			charSize: 30,
 			charSpacing: 35,
 			lineHeight: 40,
